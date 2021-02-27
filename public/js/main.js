d9cbd14cb07d40607111d283fa43ad29e2c12ccc
ca1c <gegs921@gmail.com>
2021-02-27 01:27:06 -0800

    basic functionality without cookies

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,7 @@ function findThree(three) {
             if(typeof(sequence[i + 1]) !== "undefined" && typeof(sequence[i + 2]) !== "undefined") {
 
                 if(sequence[i + 1] === three[1] && sequence[i + 2] === three[2]) {
-                    return sequence[i + 3];
+                    return [i + 3, sequence[i + 3]];
                 }
 
             } 
@@ -14,13 +14,12 @@ function findThree(three) {
                 let second;
                 let third;
 
-                if(sequence.indexOf())
                 second = typeof(sequence[i + 1]) === "undefined" ? sequence[0] : sequence[i + 1];
                 third = typeof(sequence[i + 2]) === "undefined" ? typeof(sequence[i + 1]) === "undefined" ? sequence[1] : sequence[0] : sequence[i + 2];
 
                 if(second === three[1] && third === three[2]) {
                     let finalIndex = sequence.indexOf(third);
-                    return sequence[finalIndex + 1];
+                    return [finalIndex + 1, sequence[finalIndex + 1]];
                 }
             }
         }
@@ -33,12 +32,50 @@ const app = new Vue({
         obs1: "",
         obs2: "",
         obs3: "",
+        sequence: [6, 0, 4, 5, 1, 3, 2, 0, 0, 5, 1, 0, 3, 4, 0, 0, 5, 0, 1, 0],
         nextGem: "",
+        nextGemIndex: "",
+        sequenceNotSynced: true,
+        src: "tree2.webp",
+        img: 1,
+        transitioning: false,
     },
     methods: {
         findNextGem: function() {
-            this.nextGem = findThree([parseInt(this.obs1), parseInt(this.obs2), parseInt(this.obs3)]);
+            let nextGem = findThree([parseInt(this.obs1), parseInt(this.obs2), parseInt(this.obs3)]);
+            this.nextGem = nextGem[1];
+            this.nextGemIndex = nextGem[0];
             console.log(this.obs1);
-        }
+        },
+        checkInputSeq: function() {
+            if(this.obs1 !== "" && this.obs2 !== "" && this.obs3 !== "" && this.sequenceNotSynced === true) {
+                this.findNextGem();
+                this.sequenceNotSynced = false;
+            }
+        },
+    },
+    created: function(){
+        let vm = this;
+        window.addEventListener('keyup', function(e) {
+            if(e.keyCode == 32) {
+                if(vm.sequenceNotSynced !== true && vm.transitioning !== true) {
+                    vm.transitioning = true;
+                    vm.nextGem = vm.nextGemIndex === 19 ? vm.sequence[0] : vm.sequence[vm.nextGemIndex + 1];
+                    vm.nextGemIndex = vm.nextGemIndex === 19 ? 0 : vm.nextGemIndex + 1;
+
+                    let srcs = ["bush", "mushroom", "tree1", "tree2", "tree3", "trunk_fallen", "trunk"];
+
+                    vm.src = srcs[Math.floor(Math.random() * srcs.length)] + ".webp";
+
+                    let imgPrev = vm.img;
+                    vm.img = 2;
+
+                    setTimeout(function() {
+                        vm.img = imgPrev === 1 ? 3 : 1;
+                        vm.transitioning = false;
+                    }, 500);
+                }
+            }
+        })
     }
 })
